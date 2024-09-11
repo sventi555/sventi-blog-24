e7b75160780e2c73732362a769ad09cc395ee748
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2024-09-10 22:20:14 -0400

    chore: starting configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/admin",
+        destination: "/admin/index.html",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
