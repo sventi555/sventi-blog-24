064cdce111f34659b461485f11e02e45a484ecfb
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2024-09-10 22:49:51 -0400

    chore: set up posts pages

---
+++ src/pages/posts/[slug].tsx
@@ -0,0 +1,31 @@
+import path from "path";
+import fs from "fs";
+import yaml from "js-yaml";
+import { GetStaticProps } from "next";
+
+const postsDir = path.join(__dirname, "../../../_posts");
+
+export const getStaticPaths = async () => {
+  const postSlugs = fs.readdirSync(postsDir);
+
+  return {
+    paths: postSlugs.map((slug) => slug.slice(0, -4)),
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const slug = context.params!.slug;
+
+  const postPath = path.join(postsDir, `${slug}.yml`);
+  const postYml = fs.readFileSync(postPath, "utf-8");
+  const post = yaml.load(postYml);
+
+  return { props: { post } };
+};
+
+const Page: React.FC<{ post: { title: string } }> = ({ post }) => {
+  return <div>{post.title}</div>;
+};
+
+export default Page;
